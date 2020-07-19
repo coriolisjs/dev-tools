1bfb70a358495ba2622a4a0ec02d632c915a6244
Nicolas Deveaud <nicolas@deveaud.fr>
2020-07-19 23:58:43 +0200

    fixup! Fix based on svelte builder warning

--- src/components/units/EventListItem.svelte
+++ src/components/units/EventListItem.svelte
@@ -106,7 +106,7 @@
   >
     <select
       class="timing-select"
-      on:blur={selectTimingType}
+      on:change={selectTimingType}
     >
       <option value="deltaN" selected={$selectedTimingType$ === 'deltaN'}>+{item.deltaN}ms</option>
       <option value="delta0" selected={$selectedTimingType$ === 'delta0'}>+{item.delta0}ms</option>
