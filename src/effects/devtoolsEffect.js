55e5ecd961d0eb3824e4c76e4e85b215a26a3ef3
Nicolas Deveaud <nicolas@deveaud.fr>
2021-06-23 23:39:59 +0200

    Improves devtoolsEffect readability

--- src/effects/devtoolsEffect.js
+++ src/effects/devtoolsEffect.js
@@ -5,30 +5,31 @@ import { storeAdded, storeEnded, storeEvent } from '../events/tracking/store'
 
 export const createDevtoolsEffect = (storeName = 'unnamed') => {
   const trackingSubject = new Subject()
+  const devtoolsEffect = ({ event$, pastEvent$, withProjection }) => {
+    trackingSubject.next(
+      storeAdded({
+        storeName,
+        snapshot$: withProjection(snapshot),
+      }),
+    )
+
+    const pastEventsSubscription = pastEvent$.subscribe((event) =>
+      trackingSubject.next(storeEvent({ event, isPastEvent: true })),
+    )
+
+    const eventsSubscription = event$.subscribe((event) =>
+      trackingSubject.next(storeEvent({ event })),
+    )
+
+    return () => {
+      trackingSubject.next(storeEnded())
+      pastEventsSubscription.unsubscribe()
+      eventsSubscription.unsubscribe()
+    }
+  }
 
   return {
     tracking$: trackingSubject,
-    devtoolsEffect: ({ event$, pastEvent$, withProjection }) => {
-      trackingSubject.next(
-        storeAdded({
-          storeName,
-          snapshot$: withProjection(snapshot),
-        }),
-      )
-
-      const pastEventsSubscription = pastEvent$.subscribe((event) =>
-        trackingSubject.next(storeEvent({ event, isPastEvent: true })),
-      )
-
-      const eventsSubscription = event$.subscribe((event) =>
-        trackingSubject.next(storeEvent({ event })),
-      )
-
-      return () => {
-        trackingSubject.next(storeEnded())
-        pastEventsSubscription.unsubscribe()
-        eventsSubscription.unsubscribe()
-      }
-    },
+    devtoolsEffect,
   }
 }
